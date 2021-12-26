11dbcb544c0c4d283014899295799a824cf31895
RackAnatoly <82027973+RackAnatoly@users.noreply.github.com>
2021-12-26 23:38:49 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,26 @@ import Accordion from "./components/Accordion/Accordion";
 import Rating from "./components/Rating/Rating";
 
 function App() {
-    return ( 
+    return (
         <div>
-            <PageTitle title={'This is APP component'}/>
-            <PageTitle title={'My Friends'}/>
+            <PageTitle title={'This is APP component'} />
+            <PageTitle title={'My Friends'} />
             Article 1
-            <Rating value={3}/>
-            <Accordion title={'Menu 1'}/>
-            <Accordion title={'Menu 2'}/>
+            <Rating value={3} />
+            <Accordion title={'Menu 1'} />
+            <Accordion title={'Menu 2'} />
             Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={0} />
+            <Rating value={1} />
+            <Rating value={2} />
+            <Rating value={3} />
+            <Rating value={4} />
+            <Rating value={5} />
         </div>
     );
 }
 
-const PageTitle = (props:any) => {
+const PageTitle = (props: any) => {
     return <h1>{props.title}</h1>
 }
 
@@ -32,4 +32,5 @@ const PageTitle = (props:any) => {
 
 export default App;
 
-///////
+/////////////////
+////////////////
